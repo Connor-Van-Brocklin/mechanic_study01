0596018b174225b4b014e4bff0d753366782ce2f
Connor Van Brocklin <90352499+Connor-Van-Brocklin@users.noreply.github.com>
2026-07-23 18:30:05 +0000

    Added variables and an empty bag

--- inventory.cpp
+++ inventory.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-  std::cout << "Hello World!";
+  std::vector<std::string> inv;
+  std::string item = "Legendary Sword";
+
+  std::cout << "==========================================\nInventory:\n==========================================\n";
+  std::cout << "==========================================\nCurrency:\n==========================================\nGold: 00\nSilver: 00\nCopper: 00";
 }
